5981e7d7fcaa2e9580cd24fc081a3ba5f4265a95
anaBeatrizWalker <anabiawalker@gmail.com>
2023-03-08 11:13:10 -0300

    Refatorando código: adicionando herança

--- src/br/com/desafio/dominio/Mentoria.java
+++ src/br/com/desafio/dominio/Mentoria.java
@@ -2,25 +2,11 @@ package br.com.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
     
-    private String titulo;
-    private String descricao;
     private LocalDate data;
 
     public Mentoria() {
-    }
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
     public LocalDate getData() {
         return data;
@@ -31,9 +17,13 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
+    }
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20.0;
     }
 }
